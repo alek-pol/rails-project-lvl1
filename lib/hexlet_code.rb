9df196e19eb8b6d51f9ddf8b2899ce28f8de1620
alek_pol <a.polyanskiy@outlook.com>
2022-02-22 01:49:54 +0200

    add form_for (task 4)

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -5,4 +5,12 @@ require_relative 'hexlet_code/tag'
 require_relative 'hexlet_code/version'
 
 # Form generator
-module HexletCode; end
+module HexletCode
+  # Empty form generator
+  # @param [Struct] _ entity objects
+  # @param [String] url url
+  # @return [String]
+  def self.form_for(_, url: '#')
+    Tag.build(:form, action: url, method: :post)
+  end
+end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -3,7 +3,16 @@
 require 'test_helper'
 
 class TestHexletCode < Minitest::Test
+  def setup
+    @user = Struct.new(:name, :job, keyword_init: true).new name: 'Rob'
+  end
+
   def test_that_it_has_a_version
     refute_nil ::HexletCode::VERSION
+  end
+
+  def test_form_for
+    assert_equal(HexletCode.form_for(@user), '<form action="#" method="post"></form>')
+    assert_equal(HexletCode.form_for(@user, url: '/users'), '<form action="/users" method="post"></form>')
   end
 end
